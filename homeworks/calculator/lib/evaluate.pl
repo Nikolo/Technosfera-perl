d09ecd78babf1e4412f6f8a4a75fb4ac3453acb8
Overwatcher <nazvyacheslav@gmail.com>
2016-03-07 02:22:12 +0300

    done

--- homeworks/calculator/lib/evaluate.pl
+++ homeworks/calculator/lib/evaluate.pl
@@ -31,32 +31,32 @@ sub evaluate {
 			}
 			when ('U+') { next;}
 			when ('^') {
-				if (@stack && $stack[$#stack] =~ /\-?\d+|\-?\d*.\d+/ && $#stack >= 1 && $stack[$#stack-1] =~ /\-?\d+|\-?\d*.\d+/) {
+				if (&& $stack[$#stack] =~ /\-?\d+|\-?\d*.\d+/ && $stack[$#stack-1] =~ /\-?\d+|\-?\d*.\d+/) {
 					splice (@stack, $#stack-1, 2, $stack[$#stack-1]**$stack[$#stack]);
 				} else {return 'Err';}
 				next;
 			}
 			when ('*') {
-				if (@stack && $stack[$#stack] =~ /\-?\d+|\d*.\d+/ && $#stack >= 1 && $stack[$#stack-1] =~ /\-?\d+|\d*.\d+/) {
+				if ($#stack >= 1 && $stack[$#stack] =~ /\-?\d+|\d*.\d+/ && $stack[$#stack-1] =~ /\-?\d+|\d*.\d+/) {
 					splice (@stack, $#stack-1, 2, $stack[$#stack-1]*$stack[$#stack]);
 				} else {return 'Err';}
 				next;
 			}
 			when ('/') {
-				if (@stack && $stack[$#stack] =~ /\-?\d+|\-?\d*.\d+/ && $#stack >= 1 && $stack[$#stack-1] =~ /\-?\d+|\-?\d*.\d+/) {
+				if ($#stack >= 1 && $stack[$#stack] =~ /\-?\d+|\-?\d*.\d+/ && $stack[$#stack-1] =~ /\-?\d+|\-?\d*.\d+/) {
 					if ($stack[$#stack] == 0) {return 'NaN';} 
 					splice (@stack, $#stack-1, 2, $stack[$#stack-1]/$stack[$#stack]);
 				} else {return 'Err';}
 				next;
 			}
 			when ('+') {
-				if (@stack && $stack[$#stack] =~ /\-?\d+|\-?\d*.\d+/ && $#stack >= 1 && $stack[$#stack-1] =~ /\-?\d+|\-?\d*.\d+/) {
+				if ($#stack >= 1 && $stack[$#stack] =~ /\-?\d+|\-?\d*.\d+/ && $stack[$#stack-1] =~ /\-?\d+|\-?\d*.\d+/) {
 					splice (@stack, $#stack-1, 2, $stack[$#stack-1]+$stack[$#stack]);
 				} else {return 'Err';}
 				next;
 			}
 			when ('-') {
-				if (@stack && $stack[$#stack] =~ /\-?\d+|\d*.\d+/ && $#stack >= 1 && $stack[$#stack-1] =~ /\-?\d+|\-?\d*.\d+/) {
+				if ($#stack >= 1 && $stack[$#stack] =~ /\-?\d+|\d*.\d+/ && $stack[$#stack-1] =~ /\-?\d+|\-?\d*.\d+/) {
 					splice (@stack, $#stack-1, 2, $stack[$#stack-1]-$stack[$#stack]);
 				} else {return 'Err';}
 				next;
