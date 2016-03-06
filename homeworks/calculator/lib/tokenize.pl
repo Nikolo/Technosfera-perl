e9db3423728fddcd1c5a88b8a7876b1972eb1b91
Overwatcher <nazvyacheslav@gmail.com>
2016-03-07 02:27:58 +0300

    done

--- homeworks/calculator/lib/tokenize.pl
+++ homeworks/calculator/lib/tokenize.pl
@@ -26,12 +26,39 @@ BEGIN{
 no warnings 'experimental';
 
 sub tokenize {
-	chomp(my $expr = shift);
-	my @res;
-
-	# ...
-
-	return \@res;
+	my @tokenedres;
+	my $str = shift;
+	my $it = 0;
+	my $itres = 0;
+	my $flag = 1;
+	$str =~ s/\s*//g;
+	my @tokened1 = split m{([\/\(\)\^\+\-\*])}, $str; # could be '' in case of operators standing together
+	$str =~ s/\s*//g;
+	while ($it <= $#tokened1) {
+		if ($tokened1[$it] =~ /e$/) {
+			$flag = 0;
+			if (($tokened1[$it+1] =~ /\+|\-/) && ($tokened1[$it+2] =~ /\d+/)) {
+				$tokenedres[$itres] = join('', $tokened1[$it], $tokened1[$it+1], $tokened1[$it+2]);
+				$it += 3;
+				$itres++;
+				$flag = 1;
+				next;
+			}
+			if ($tokened1[$it+1] =~/\d+/) {
+				$tokenedres[$itres] = join('', $tokened1[$it], $tokened1[$it+1]);
+				$it += 2;
+				$itres++;
+				$flag = 1;
+				next;
+			}
+			return undef if !$flag;
+		}
+	$tokenedres[$itres] = $tokened1[$it];
+	$it++;
+	$itres++;
+	}
+	return @tokenedres if wantarray;
+	1;
 }
 
 1;
