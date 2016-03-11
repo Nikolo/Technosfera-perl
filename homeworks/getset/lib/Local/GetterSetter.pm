9e32ea3a14fbd7a040462b5707849bf619a65e71
Overwatcher <nazvyacheslav@gmail.com>
2016-03-11 22:10:35 +0300

    Update GetterSetter.pm

--- homeworks/getset/lib/Local/GetterSetter.pm
+++ homeworks/getset/lib/Local/GetterSetter.pm
@@ -2,19 +2,19 @@ package Local::GetterSetter;
 
 use strict;
 use warnings;
-no strict 'refs';
 
 sub import {
-(my $package, my $filename, my $line) = caller(1);
-for my $key (@_) {
-	my $strset = $package.'::'."set_$key";
-	*{$strset} = sub {
-		${*{$package.'::'.$key}} = shift;
-	};
-	my $strget = $package.'::'."get_$key";
-	*{$strget} = sub {
-		  return ${*{$package.'::'.$key}};
-	};
+	no strict 'refs';
+	my ($package, $filename, $line) = caller();
+	for my $key (@_) {
+		print "$key\n";
+		*{$package.'::'."set_$key"} = sub {
+			${$package.'::'.$key} = shift;
+		};
+		
+		*{$package.'::'."get_$key"} = sub {
+		  	return ${$package.'::'.$key};
+		};
 	}
 }
 1;
