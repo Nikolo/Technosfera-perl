4eb48a385a6fd24dd4e6d103419a728413283919
VadimPushtaev <pushtaev.vm@gmail.com>
2016-04-07 12:27:32 +0300

    hw/habr

---
+++ homeworks/habr/lib/Local/Habr.pm
@@ -0,0 +1,24 @@
+package Local::Habr;
+
+use strict;
+use warnings;
+
+=encoding utf8
+
+=head1 NAME
+
+Local::Habr - habrahabr.ru crawler
+
+=head1 VERSION
+
+Version 1.00
+
+=cut
+
+our $VERSION = '1.00';
+
+=head1 SYNOPSIS
+
+=cut
+
+1;
