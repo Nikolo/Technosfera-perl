f33fd0aa2d154c4a76a3239ea17c7c01921e6063
ermakovvova <ermakovvova@mail.r>
2017-03-12 13:35:58 -0700

    SecretSanta.pm changed

--- homeworks/santa/lib/SecretSanta.pm
+++ homeworks/santa/lib/SecretSanta.pm
@@ -5,13 +5,30 @@ use strict;
 use warnings;
 use DDP;
 
+#  1 Рита Игорь
+#  2 Вадим Катя
+#  3 Али
+#  4 Ира
+
+
 sub calculate {
-	my @members = @_;
+#	my @members = @_;
+	my $ref1 = ['Рита', 'Игорь'];
+	my $ref2 = ['Вадим', 'Катя'];
+	my @members = ('Али', 'Ира', $ref1, $ref2); 
 	my @res;
+	for my $var (0..$#members + scalar grep { ref($_) } @members) {
+		print "$var\n";  
+	}
 	# ...
 	#	push @res,[ "fromname", "toname" ];
 	# ...
-	return @res;
+	#return @res;
+	print "@members\n";
+	my %hash = map {@$_} grep{ ref($_) } @members;
+	print values %hash;
+
 }
+calculate;
 
 1;
