134ce5af17a793733c3fb68f605f8fb7145988c4
VadimPushtaev <pushtaev.vm@gmail.com>
2017-03-20 19:22:20 +0300

    live width

--- live/music_library_2017_03_20/lib/Local/PrettyTable.pm
+++ live/music_library_2017_03_20/lib/Local/PrettyTable.pm
@@ -5,10 +5,56 @@ use warnings;
 
 use Mouse;
 
+use List::Util qw(max sum);
+
 has data => (
     is => 'ro',
     isa => 'ArrayRef[ArrayRef[Str]]',
     required => 1,
 );
+
+has _cols_width => (
+    is => 'ro',
+    isa => 'ArrayRef[Int]',
+    required => 1,
+    lazy => 1,
+    builder => '_build__cols_width',
+);
+
+has _width => (
+    is => 'ro',
+    isa => 'Int',
+    required => 1,
+    lazy => 1,
+    builder => '_build__width',
+);
+
+sub _build__cols_width {
+    my ($self) = @_;
+
+    my $result = [];
+
+    foreach my $row (@{$self->data}) {
+        for my $col_n (0 .. @{$row} - 1) {
+            my $len = length($row->[$col_n]);
+            $result->[$col_n] = max($result->[$col_n] // 0, $len);
+        }
+    }
+
+    return $result;
+}
+
+sub _build__width {
+    my ($self) = @_;
+
+    my @cols_width = @{$self->_cols_width};
+    my $margins = 2 * @cols_width;
+    my $borders = @cols_width + 1;
+
+    return $margins + $borders + sum(@cols_width);
+}
+
+sub to_string {
+}
 
 1;
--- live/music_library_2017_03_20/t/lib/Local/PrettyTable/Test.pm
+++ live/music_library_2017_03_20/t/lib/Local/PrettyTable/Test.pm
@@ -4,16 +4,52 @@ use Test::Class::Moose;
 
 use Local::PrettyTable;
 
+sub _sample {
+    return [
+        ['bamby', 4, 'moose'],
+        ['sonic', 2, 'hedgehog'],
+        ['dumbledore', 100, 'human'],
+    ],
+}
+
 sub test_new {
     my ($self) = @_;
 
     my $table = Local::PrettyTable->new(
-        data => [
-            [], [], [],
-        ],
+        data => $self->_sample(),
     );
 
     ok($table);
+
+    return;
+}
+
+sub test__cols_width {
+    my ($self) = @_;
+
+    my $table = Local::PrettyTable->new(
+        data => $self->_sample(),
+    );
+
+    cmp_deeply(
+        $table->_cols_width,
+        [10, 3, 8],
+    );
+
+    return;
+}
+
+sub test__width {
+    my ($self) = @_;
+
+    my $table = Local::PrettyTable->new(
+        data => $self->_sample(),
+    );
+
+    is(
+        $table->_width,
+        31,
+    );
 
     return;
 }
