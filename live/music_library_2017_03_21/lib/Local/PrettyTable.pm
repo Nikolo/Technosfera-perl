a800f70adfed2c782f71ed6d6031836f99a8e77a
VadimPushtaev <pushtaev.vm@gmail.com>
2017-03-21 18:55:56 +0300

    live header scaffold

--- live/music_library_2017_03_21/lib/Local/PrettyTable.pm
+++ live/music_library_2017_03_21/lib/Local/PrettyTable.pm
@@ -11,4 +11,32 @@ has matrix => (
     required => 1,
 );
 
+sub cols_width {
+    my ($self) = @_;
+
+    return;
+}
+
+sub width {
+    my ($self) = @_;
+
+    foreach my $row (@{$self->matrix}) {
+        
+    }
+
+    return;
+}
+
+sub header {
+    my ($self) = @_;
+
+    return;
+}
+
+sub to_string {
+    my ($self) = @_;
+
+    return;
+}
+
 1;
--- live/music_library_2017_03_21/t/lib/Local/PrettyTable/Test.pm
+++ live/music_library_2017_03_21/t/lib/Local/PrettyTable/Test.pm
@@ -16,4 +16,36 @@ sub test_new {
     return;
 }
 
+sub test_cols_width {
+    my ($self) = @_;
+
+    my $matrix = [
+        ['mama',    'myla',        'ramu'],
+        ['uronili', 'mishku',      'na pol'],
+        ['foo',     'integration', 'bar'],
+    ];
+
+    my $table = Local::PrettyTable->new(matrix => $matrix);
+    
+    cmp_deeply($table->cols_width, [7, 11, 6]);
+
+    return;
+}
+
+sub test_width {
+    my ($self) = @_;
+
+    my $matrix = [
+        ['mama',    'myla',        'ramu'],
+        ['uronili', 'mishku',      'na pol'],
+        ['foo',     'integration', 'bar'],
+    ];
+
+    my $table = Local::PrettyTable->new(matrix => $matrix);
+    
+    is($table->width, 34);
+
+    return;
+}
+
 1;
