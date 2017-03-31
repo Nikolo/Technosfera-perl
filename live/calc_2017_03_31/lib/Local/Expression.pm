48aa3a8a4a7d5f5bc43b375bcda0757dbaca1870
VadimPushtaev <pushtaev.vm@gmail.com>
2017-03-31 18:47:41 +0300

    live: get tokens

--- live/calc_2017_03_31/lib/Local/Expression.pm
+++ live/calc_2017_03_31/lib/Local/Expression.pm
@@ -12,4 +12,35 @@ has string => (
     required => 1,
 );
 
+sub _get_tokens {
+    my ($self) = @_;
+
+    my @result;
+
+    while ($self->string =~ m{
+        \G ( \d+    )|
+        \G ( [+*/-] )|
+        \G ( \s+    )|
+        \G ( .+     )
+    }xg) {
+        if (length($1)) {
+            push(@result, $1);
+        }
+        elsif (length($2)) {
+            push(@result, $2);
+        }
+        elsif (length($3)) {
+            # spaces
+        }
+        elsif (length($4)) {
+            die "Don't know how to parse $4";
+        }
+        else {
+            die q{Shouldn't be here};
+        }
+    }
+
+    return \@result;
+}
+
 1;
--- live/calc_2017_03_31/t/lib/Local/Expression/Test.pm
+++ live/calc_2017_03_31/t/lib/Local/Expression/Test.pm
@@ -16,4 +16,45 @@ sub test_new {
     return;
 }
 
+sub test__get_tokens {
+    my ($self) = @_;
+
+    my $expression = Local::Expression->new(
+        string => '22 + 2 * 42-1',
+    );
+
+    cmp_deeply(
+        $expression->_get_tokens(),
+        [qw(
+            22
+            +
+            2
+            *
+            42
+            -
+            1
+        )]
+    );
+
+    return;
+}
+
+sub test__get_tokens__bad {
+    my ($self) = @_;
+
+    my $expression = Local::Expression->new(
+        string => '22 + abc',
+    );
+
+    eval {
+        $expression->_get_tokens();
+        1;
+    } or do {
+        like($@, qr{Don't know how to parse});
+        return;
+    };
+
+    fail('Should die');
+}
+
 1;
