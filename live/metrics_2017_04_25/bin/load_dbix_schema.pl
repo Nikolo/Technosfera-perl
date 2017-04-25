dacb1e0b71156f3d091b9d2df045ffdb672a85b5
VadimPushtaev <pushtaev.vm@gmail.com>
2017-04-25 19:16:17 +0300

    live: config class

--- live/metrics_2017_04_25/bin/load_dbix_schema.pl
+++ live/metrics_2017_04_25/bin/load_dbix_schema.pl
@@ -3,12 +3,13 @@
 use strict;
 use warnings;
 
+use FindBin;
 use DBIx::Class::Schema::Loader qw(make_schema_at);
 
 make_schema_at(
     'Local::Metric::Schema',
     {
-        dump_directory => './lib',
+        dump_directory => $FindBin::Bin . '/../lib',
     },
     [
         'DBI:mysql:database=metrics', 
